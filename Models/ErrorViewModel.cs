a5a495748d3e2e815237eab830af0ae3f6f0ac9f
Vineet Yadav <Vineet.Yadav@bd.com>
2020-07-03 02:43:07 +0530

    baseline

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dnn.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
